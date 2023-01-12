daf8eba9ef4c837f3c80f79f6c7ee01b5eb4fae3
wagbrito <wagnerbritodejesus@gmail.com>
2023-01-11 21:15:09 -0300

    Nessa nova versão eu consegui reduzir o tamanho do IF, deixando até mais prático.

--- jokenpo.py
+++ jokenpo.py
@@ -9,54 +9,35 @@ placar_computador = 0
 itens = ('Papel', 'Pedra', 'Tesoura')
 
 while True:
+    os.system('cls')
     print('='*47)
     print('Bem vindo(a) ao jogo do Pedra, Papel ou Tesoura')
     print('='*47)
     print()
-    print('Placar')
+    print('PLACAR')
     print('Você: {}'.format(placar_jogador))
     print('Computador: {}'.format(placar_computador))
     print()
     print('0 - Papel | 1 - Pedra | 2 - Tesoura')
-    
+    print()
     jogador = int(input('Qual a sua jogada? '))
     computador = random.randint(0,2)
-    
+    print()
     print('Sua jogada: {}'.format(itens[jogador]))
     print('Jogada do computador: {}'.format(itens[computador]))
+    print()
 
-    if jogador == 0: #se o jogador jogar Papel
-        if computador == 0: #se o computador jogar Papel
-            print('Empate')
-        elif computador == 1: #se o computador jogar Pedra
-            print('O jogador vence')
-            placar_jogador = placar_jogador + 1
-        elif computador == 2: #se o computador jogar Tesoura
-            print('O computador vence')
-            placar_computador = placar_computador + 1
-    
-    if jogador == 1: #se o jogador jogar Pedra
-        if computador == 1: #se o computador jogar Pedra
-            print('Empate')
-        elif computador == 2: #se o computador jogar Tesoura
-            print('O jogador vence')
-            placar_jogador = placar_jogador + 1
-        elif computador == 0: #se o computador jogar Papel
-            print('O computador vence')
-            placar_computador = placar_computador + 1
-              
-    if jogador == 2: #se o jogador jogar Tesoura
-        if computador == 2: #se o computador jogar Tesoura
-            print('Empate')
-        elif computador == 0: #se o computador jogar Papel
-            print('O jogador vence')
-            placar_jogador = placar_jogador + 1
-        elif computador == 1: #se o computador jogar Pedra
-            print('O computador vence')
-            placar_computador = placar_computador + 1
+    if jogador == computador: #se o jogador e computador fizerem a mesma escolha
+        print('Empate. Ninguém pontua')
+    elif (jogador == 0 and computador == 2) or (jogador == 1 and computador == 0) or (jogador == 2 and computador == 1):
+        print('O computador vence')
+        placar_computador = placar_computador + 1
+    else:
+        print('O jogador vence')
+        placar_jogador = placar_jogador + 1        
 
     print('')
-    print('='*30)
+    print('='*35)
     print('Jogar novamente? 0 - SIM | 1 - NÃO')
     if int(input()) == 1:
-        break
+        break  
